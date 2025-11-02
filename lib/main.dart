306495c966f15d12c471b08c8bf86d718a5a8555
zaqimaulana <zaqimaulana72@gmail.com>
2025-11-03 02:46:22 +0700

    update..

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: "Nim : 1123150048 , zaqi", 
+      theme: ThemeData(
+        useMaterial3: true, 
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: const SplashScreen1(), 
+    );
   }
 }
