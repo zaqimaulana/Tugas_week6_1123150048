58a678eedf587c26f2ecf8acb12553415a723fe0
zaqimaulana <zaqimaulana72@gmail.com>
2025-11-03 21:53:19 +0700

    Perbaikan: menghubungkan splash screen dengan halaman utama

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'screen/splash_screen_1.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Nim : 1123150048 , zaqi", 
+      title: "Nim : 1123150048, zaqi",
       theme: ThemeData(
-        useMaterial3: true, 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
       ),
-      home: const SplashScreen1(), 
+      home: const SplashScreen1(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
+
