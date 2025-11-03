c09277564d58319eaf8992369cae603efeb6627e
zaqimaulana <zaqimaulana72@gmail.com>
2025-11-04 00:08:29 +0700

    mengubah posisi lingkaran jadi ditengah dan menambahkan gambar di assets

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -6,28 +6,31 @@ class SplashScreen1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            width: 250,
-            height: 250,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.amber,
+      body: Center( 
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 250,
+              height: 250,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.amber,
+              ),
             ),
-          ),
 
-          // TODO:
-          // karena akan membuat multiple widget
-          // maka membuat children yang bisa menampung banyak
-          // widget "children"
-          // membuat Image bulat
-          // Membuat Title dengan warna hitam
-          // Membuat subtitle
-          // Membuat 3 bullet kecil
-          // mulai dari warna terang diawal
-          // Membuat Button Continue
-        ],
+            // TODO:
+            // karena akan membuat multiple widget
+            // maka membuat children yang bisa menampung banyak
+            // widget "children"
+            // membuat Image bulat
+            // Membuat Title dengan warna hitam
+            // Membuat subtitle
+            // Membuat 3 bullet kecil
+            // mulai dari warna terang diawal
+            // Membuat Button Continue
+          ],
+        ),
       ),
     );
   }
