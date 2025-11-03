376919444e4526649bf86acc9616e36efdf7ebcd
zaqimaulana <zaqimaulana72@gmail.com>
2025-11-04 03:54:13 +0700

    membuat scaffold login

---
+++ lib/screen/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: const [
+              SizedBox(height: 50),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
